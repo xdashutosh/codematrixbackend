55841b4037c7f4d5ee9b12cef3ddce82f88a726d
ashutosh chaudhary <ashutosh.ch@damsdelhi.com>
2024-05-23 13:08:55 +0530

    cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000','https://codematrix-one.vercel.app'],
     credentials:true
   };
   
